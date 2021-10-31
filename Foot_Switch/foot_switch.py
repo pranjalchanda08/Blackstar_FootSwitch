d0b3722d9d911e52f720b38e3f7ff02ea554e893
Pranjal Chanda <pranjalchanda08@gmail.com>
2021-10-31 12:28:44 +0530

    Default Patch init

--- Foot_Switch/foot_switch.py
+++ Foot_Switch/foot_switch.py
@@ -1,18 +1,33 @@
 from blackstarid import BlackstarIDAmp, NoDataAvailable, NotConnectedError
 import sys
 import RPi.GPIO as GPIO
-import signal
 import logging
 import threading
+import json
+from  beeprint import pp
 
 class foot_switch():
     '''
     '''
-    FS_BUTTON_LIST = [5, 6, 13, 19, 26]
-    
+    FS_BUTTON_DICT = {
+        "Pins":{
+            "FS_PATCH_UP": 5,
+            "FS_PATCH_DOWN": 6,
+            "FS_MOD_TOGGLE": 13,
+            "FS_DEL_TOGGLE": 19,
+            "FS_REV_TOGGLE": 26,
+        },        
+        "Control" : {}
+    }
     def __init__(self):
+        with open('patch.json', 'r') as patch_file:
+            file_dict = json.load(patch_file)
+            for patch in file_dict['patches']:
+                if file_dict['default'] == patch['name']:
+                    print("Default patch: " + patch['name'])
+                    self.FS_BUTTON_DICT['Control'] = patch['Control']
         GPIO.setmode(GPIO.BCM)
-        for button in self.FS_BUTTON_LIST:
+        for button in list(self.FS_BUTTON_DICT['Pins'].values()):
             GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_UP)
             GPIO.add_event_detect(button, GPIO.BOTH, callback = self.fs_but_callback, bouncetime=100)
         try:
@@ -25,10 +40,14 @@ class foot_switch():
                 self.bs.connect()
                 self.bs.drain()
                 self.bs.startup()
-                self.read_thread = threading.Thread(target=self.read_thread_entry, args=(1,))
                 self.alive = True
-                self.control = {}
+                self.read_thread = threading.Thread(target=self.read_thread_entry, args=(1,))
                 self.read_thread.start()
+                for control in self.FS_BUTTON_DICT['Control']:
+                    print(control)
+                    if control == "delay_time":
+                        continue
+                    self.bs.set_control(control, self.FS_BUTTON_DICT['Control'][control])
                 
         except NotConnectedError:
             raise NotConnectedError
@@ -43,7 +62,8 @@ class foot_switch():
     def read_thread_entry(self, name):
         while self.alive:
             try:
-                self.control = self.bs.read_data()
+                self.FS_BUTTON_DICT['Control'] = self.bs.read_data()
+                logging.debug(self.FS_BUTTON_DICT['Control'])
             except NoDataAvailable:
                 pass
             
@@ -51,22 +71,25 @@ class foot_switch():
     def fs_but_callback(self, channel):
         if GPIO.input(channel):
             return
-        if channel == self.FS_BUTTON_LIST[0]:
-            self.bs.set_control('isf', 125)
-        elif channel == self.FS_BUTTON_LIST[1]:
-            pass
-        elif channel == self.FS_BUTTON_LIST[2]:
-            pass
-        elif channel == self.FS_BUTTON_LIST[3]:
-            pass
-        elif channel == self.FS_BUTTON_LIST[4]:
+        
+        if channel == self.FS_BUTTON_DICT['Pins']['FS_PATCH_UP']:
             pass
         
-     
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
-       
+        elif channel == self.FS_BUTTON_DICT['Pins']['FS_PATCH_DOWN']:
+            pass
+        
+        elif channel == self.FS_BUTTON_DICT['Pins']['FS_MOD_TOGGLE']:
+            self.FS_BUTTON_DICT['Control']['mod_switch'] ^= 1;
+            self.bs.set_control('mod_switch', self.FS_BUTTON_DICT['Control']['mod_switch'])
+            
+        elif channel == self.FS_BUTTON_DICT['Pins']['FS_DEL_TOGGLE']:
+            self.FS_BUTTON_DICT['Control']['delay_switch'] ^= 1;
+            self.bs.set_control('delay_switch', self.FS_BUTTON_DICT['Control']['delay_switch'])
+        
+        elif channel == self.FS_BUTTON_DICT['Pins']['FS_REV_TOGGLE']:
+            self.FS_BUTTON_DICT['Control']['reverb_switch'] ^= 1;
+            self.bs.set_control('reverb_switch', self.FS_BUTTON_DICT['Control']['reverb_switch'])
+            
 if __name__ == '__main__':
     
     try:
