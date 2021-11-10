0d6eb473fe313335cfc56f83b07a033f7a1b0d44
Pranjal <pranjalchanda08@gmail.com>
2021-11-10 10:31:10 +0000

    Latest changes

--- env_setup.sh
+++ env_setup.sh
@@ -4,6 +4,8 @@ cd pyusb
 sudo python3 setup.py install
 sudo pip3 install Adafruit_SSD1306
 sudo pip3 install paho-mqtt
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --node16 --confirm-pi --restart
-npm install node-red-dashboard
-npm install node-red-contrib-python3-function
+#bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --node16 --confirm-pi --restart
+#npm install node-red-dashboard
+#npm install node-red-contrib-python3-function
+git config --global user.email "pranjalchanda08@gmail.com"
+git config --global user.name "Pranjal"
