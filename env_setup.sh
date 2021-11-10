0cd5548194fcb93f5dd0be7f892bf3d341d4f39e
Pranjal Chanda <pranjal.chanda@havells.com>
2021-11-10 16:16:01 +0530

    Repo cleanup

--- env_setup.sh
+++ env_setup.sh
@@ -7,5 +7,7 @@ sudo pip3 install paho-mqtt
 #bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --node16 --confirm-pi --restart
 #npm install node-red-dashboard
 #npm install node-red-contrib-python3-function
+cd ../Adafruit_Python_ILI9341
+sudo python3 setup.py install
 git config --global user.email "pranjalchanda08@gmail.com"
 git config --global user.name "Pranjal"
