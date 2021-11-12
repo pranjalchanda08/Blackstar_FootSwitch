e9622a9f9e515441aa178ebbd646b47f2bcf8b2a
Pranjal <pranjalchanda08@gmail.com>
2021-11-12 10:59:36 +0000

    TFT Dashboard Done

--- Foot_Switch/Tft.py
+++ Foot_Switch/Tft.py
@@ -7,6 +7,7 @@ import board
 
 import time
 import json
+import subprocess
 
 import Adafruit_ILI9341 as TFT
 import Adafruit_GPIO.SPI as SPI
@@ -58,6 +59,7 @@ disp = ili9341.ILI9341(
     rst=reset_pin,
     baudrate=BAUDRATE,
 )
+
 if disp.rotation % 180 == 90:
     height = disp.width  # we swap height/width to rotate it to landscape!
     width = disp.height
@@ -78,26 +80,29 @@ disp.image(image)
 padding = 4
 x = 4
 
-# Load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-# font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
-font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf", 20)
-font2 = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf", 30)
-
+pwm_flag = False
+refresh_flag = True
 display = {
-    "patch" : "default",
-    "volume" : 10,
-    "ISF" : 9, 
-    "Base" : 5,
-    "Treble" : 3
+    "voice" : 5,
+    "patch" : "Metal Low",
+    "volume" : 8,
+    "isf" : 9, 
+    "bass" : 5,
+    "treble" : 3,
+    "mod": 1,
+    "delay" : 1,
+    "reverb" : 1
 }
 def on_message(client, userdata, message):
         global display
-        msg = message.payload.decode()
-        update_param = json.loads(msg)
-        display.update(update_param)
-        print(display)
+        global refresh_flag
+        try: 
+            msg = message.payload.decode()
+            update_param = json.loads(msg)
+            display.update(update_param)
+            refresh_flag = True
+        except Exception as e:
+            print(msg + str(e))
 
 try:
     client = mqtt.Client("LocalHostDisplay")
@@ -108,57 +113,98 @@ try:
 except Exception as e:
     print(" Exception in Parent thread: " + str(e))
 
+def get_status_bar_fill(val, width_start, width_end, sections, scale:tuple):
+    scale_len = scale[1] - scale [0]
+    total_width = width_end - width_start
+    section_width = (total_width / sections) * scale_len
+    res_width = section_width * val
+    return width_start + res_width
+
+bg_color = (198, 71, 86) #(20,47,67)
+status_bar_outline = bg_color
+target_theme_color = (255, 255, 255)
+outline_color = (255, 255, 255)
+
+font = ImageFont.FreeTypeFont("font/Game Of Squids.otf", size=30)
+font2 = ImageFont.FreeTypeFont("font/PixelOperatorHB.ttf", size=20)
+# font2 = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf", 20)
+offset = 4
+text_offset = 8
+
+voice_list = ['Clean Warm', 'Clean Cool', 'Crunch', 'Super Crunch', 'Over Drive 1', 'Over Drive 2']
+
 while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=(255, 255, 255), width = 2, fill=0)
-    
-    # Shell scripts for system monitoring from here:
-    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    
-    # Write four lines of text.
-    y = padding
-    
-    vol = "Volume  :{}".format(display['volume'])
-    isf = "ISF     :{}".format(display['ISF'])
-    Base = "Base    :{}".format(display['Base'])
-    Treble = "Treble  :{}".format(display['Treble'])
-    patch =  "{}".format(display['patch'])
-    draw.text((x, y), vol, font=font, fill="#FFFF00")
-    y += font.getsize(vol)[1]
-    
-    draw.text((x, y), isf, font=font, fill="#00FF00")
-    y += font.getsize(isf)[1]
-    
-    draw.text((x, y), Base, font=font, fill="#0000FF")
-    y += font.getsize(Base)[1]
-    
-    draw.text((x, y), Treble, font=font, fill="#FF00FF")
-    y += font.getsize(Treble)[1]
-    
-    draw.text((x, y), patch, font=font2, fill="#FFFFFF")
-    y += font.getsize(patch)[1]
-    # Display image.
-    disp.image(image)
-    time.sleep(1)
-    
-# draw = disp.draw()
+    if refresh_flag:
+        image = Image.new("RGB", (width, height), bg_color)
+        draw = ImageDraw.Draw(image)
 
-# font = ImageFont.truetype('font/PixelOperator8.ttf', 30)
+        Patch = display['patch']
+        Voice = voice_list [display['voice']]
+        MOD = display['mod']
+        REV = display['reverb']
+        DEL = display['delay']
 
-# def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
-#     # Get rendered font width and height.
-#     draw = ImageDraw.Draw(image)
-#     width, height = draw.textsize(text, font=font)
-#     # Create a new image with transparent background to store the text.
-#     textimage = Image.new('RGBA', (width, height), (0,0,0,0))
-#     # Render the text.
-#     textdraw = ImageDraw.Draw(textimage)
-#     textdraw.text((0,0), text, font=font, fill=fill)
-#     # Rotate the text image.
-#     rotated = textimage.rotate(angle, expand=1)
-#     # Paste the text into the image, using it as a mask for transparency.
-#     image.paste(rotated, position, rotated)
+        draw.text(xy= (width/2,(1*height)/4),text=Patch, font=font,anchor='mm', fill= target_theme_color)
+        
+        draw.rectangle(((2*width/3 - offset/2),height/2, width, height), outline=outline_color, width=offset)
+        draw.rectangle((0,height/2, width, height), outline=outline_color, width=offset)
+        draw.rectangle((0,0, width, height), outline=outline_color, width=offset)
+        
+        Vol = "Vol      "
+        Bass = "Bass"
+        Trebble = "Treb"
+        ISF = "ISF"
 
+        status_bar_width = (2*width/3 - offset * 2)
 
-# draw_rotated_text(disp.buffer, "Your dad's name is Natesh", (90, -(10 * 10)), 90, font, fill=(255,0,255))
-# disp.display()
+        text_height_start = (height/2) + text_offset
+        draw.text(xy= (text_offset,text_height_start) ,text=Voice, font=font2, fill=target_theme_color)
+        text_height_start += draw.textsize(Voice, font=font2)[1] + 4
+
+        draw.text(xy= (text_offset,text_height_start) ,text=Vol, font=font2, fill=target_theme_color)
+        draw.text(xy= (text_offset + 10+ status_bar_width,text_height_start) ,text="MOD", font=font2,
+                        fill= target_theme_color if MOD else bg_color)
+        next_text_height = draw.textsize(Vol, font=font2)[1]
+
+        rect_width = draw.textsize(Vol, font=font2)[0]
+        fill_width = get_status_bar_fill(display['volume'], (rect_width + 1), status_bar_width, 100, scale=(0,10))
+        draw.rectangle((rect_width,text_height_start, 2*width/3 - offset * 2, text_height_start + next_text_height), outline=status_bar_outline, width=1)
+        draw.rectangle((rect_width + 1,text_height_start + 1, fill_width, text_height_start - 1 + next_text_height), fill=target_theme_color, width=1)
+        text_height_start += next_text_height
+
+        draw.text(xy= (text_offset,text_height_start) ,text=Bass, font=font2, fill=target_theme_color)
+        draw.text(xy= (text_offset + 10+ status_bar_width,text_height_start) ,text="DELAY", font=font2,
+                        fill= target_theme_color if DEL else bg_color)
+        rect_width = draw.textsize(Vol, font=font2)[0]
+        fill_width = get_status_bar_fill(display['bass'], (rect_width + 1), status_bar_width, 100, scale=(0,10))
+        draw.rectangle((rect_width,text_height_start, 2*width/3 - offset * 2, text_height_start + next_text_height), outline=status_bar_outline, width=1)
+        draw.rectangle((rect_width + 1,text_height_start + 1, fill_width, text_height_start - 1 + next_text_height), fill=target_theme_color, width=1)
+        text_height_start += draw.textsize(Bass, font=font2)[1]
+
+        draw.text(xy= (text_offset,text_height_start) ,text=Trebble, font=font2, fill=target_theme_color)
+        draw.text(xy= (text_offset + 10+ status_bar_width,text_height_start) ,text="REVERB", font=font2,
+                        fill= target_theme_color if REV else bg_color)
+
+        rect_width = draw.textsize(Vol, font=font2)[0]
+        fill_width = get_status_bar_fill(display['treble'], (rect_width + 1), status_bar_width, 100, scale=(0,10))
+        draw.rectangle((rect_width,text_height_start, 2*width/3 - offset * 2, text_height_start + next_text_height), outline=status_bar_outline, width=1)
+        draw.rectangle((rect_width + 1,text_height_start + 1, fill_width, text_height_start - 1 + next_text_height), fill=target_theme_color, width=1)
+
+        text_height_start += draw.textsize(Trebble, font=font2)[1]
+
+        draw.text(xy= (text_offset,text_height_start) ,text=ISF, font=font2, fill=target_theme_color)
+        rect_width = draw.textsize(Vol, font=font2)[0]
+        fill_width = get_status_bar_fill(display['isf'], (rect_width + 1), status_bar_width, 100, scale=(0,10))
+        draw.rectangle((rect_width,text_height_start, 2*width/3 - offset * 2, text_height_start + next_text_height), outline=status_bar_outline, width=1)
+        draw.rectangle((rect_width + 1,text_height_start + 1, fill_width, text_height_start - 1 + next_text_height), fill=target_theme_color, width=1)
+
+        text_height_start += draw.textsize(ISF, font=font2)[1]
+
+        disp.image(image)
+        if not pwm_flag:
+            time.sleep(0)
+            pwm_flag = True
+            process = subprocess.run(['gpio', '-g', 'mode', '{0}'.format(BACK_LED), 'pwm'])
+            process = subprocess.run(['gpio', '-g', 'pwm', '{0}'.format(BACK_LED), '1024'])
+        refresh_flag = False
+    
